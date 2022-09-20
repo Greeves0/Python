937806eb87302f24dc1a7a8aa03ef6df8daef42d
Greeves0 <112971820+Greeves0@users.noreply.github.com>
2022-09-20 09:52:09 +0200

    Add files via upload

---
+++ algo/tp1/tp1_ex07 corrigé.py
@@ -0,0 +1,12 @@
+# Créé par guillaume.reeves, le 20/09/2022 en Python 3.7
+
+nb_valeur=10
+result=0
+
+for n in range(1,nb_valeur+1):
+    result=result+n
+print("Somme des",nb_valeur,"premiers nombres entiers est:",result)
+
+
+
+
