c11a38fa3560d26147522bbfb24ae74c0b94d554
Greeves0 <112971820+Greeves0@users.noreply.github.com>
2022-09-20 09:34:36 +0200

    Add files via upload

---
+++ algo/tp1/boucle_pour.py
@@ -0,0 +1,10 @@
+# Créé par guillaume.reeves, le 20/09/2022 en Python 3.7
+
+for n in range(5,-6,-2):
+    print(n)
+
+for a in range(10):
+    print(a)
+
+for b in range(0,10,2):
+    print(b)
---
+++ algo/tp1/boucle_tant_que.py
@@ -0,0 +1,16 @@
+# Créé par guillaume.reeves, le 20/09/2022 en Python 3.7
+
+a=0
+while a<5: #tant que a est inférieur à 5
+    print(a) #on rest dans la boucle
+    a=a+1 #a>=5 on sort de la boucle
+
+print("fin") #print est à l'extérieur de la boucle
+
+a=10
+while a>0: #tant que a est inférieur à 5
+    print(a) #on rest dans la boucle
+    a=a-2 #a>=5 on sort de la boucle
+
+print("fin") #print est à l'extérieur de la boucle
+
