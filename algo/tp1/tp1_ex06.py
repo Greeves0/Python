b4360cbd5de813c897fbe59082e9763eb019182d
Greeves0 <112971820+Greeves0@users.noreply.github.com>
2022-09-20 09:49:28 +0200

    Add files via upload

--- algo/tp1/tp1_ex06.py
+++ algo/tp1/tp1_ex06.py
@@ -1,6 +1,6 @@
 # Créé par GUILLAUME.REEVES, le 15/09/2022 en Python 3.7
 
-age=int(input("Donner l'âge de votre animal"))
+age=int(input("Donner l'âge de votre animal")) #ou on peut directement mettre un nombre
 if(age>=6) and (age<=7):
     print("Poussin")
 elif(age>=8) and (age<=9):
