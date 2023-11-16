a74c75a89b11e12cd687933003670bfa6fe6bddb
Greeves0 <112971820+Greeves0@users.noreply.github.com>
2023-11-16 09:55:05 +0100

    Add files via upload

---
+++ 161337888921_nsi_03.py
@@ -0,0 +1,21 @@
+
+def dichotomie(tab, x):
+    debut = 0
+    fin = len(tab) - 1
+    while debut <= fin:
+        m = ...
+        if x == tab[m]:
+            return ...
+        if x > tab[m]:
+            debut = m + 1
+        else:
+             fin = ...
+    return ...
+
+
+"""
+def mult(n1,n2):
+    return n1*n2
+
+print(mult(3,5))
+"""
